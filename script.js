03dece3abdc6c60a07087d0f62006dd062fd2699
krauseangelique <krause_angelique@hotmail.com>
2023-08-04 12:53:57 +0200

    Exercices 1 et 2

--- script.js
+++ script.js
@@ -39,19 +39,55 @@ let myAlphabet = [
     "-",
     "+",
   ];
+
 // Traitement : What is the length of the array?
 console.log(myAlphabet.length); // Résultat 31
 
 // Traitement : Write a function called myAlphabetLength() which console.log the length of the array.
-function myAlphabet() {
+function forAlphabet() {
     console.log(myAlphabet.length);     
 }
 
 // Appel de fonction
-myAlphabet(); // Résultat 31
+forAlphabet(); // Résultat 31
 
 
-// Exo2 : 
+// Exo2 : Create an Array
+
+// Enoncé : Declare and initialize an array called planets with 5 string values.
+const planets = ["pluton", "mercure", "jupiter", "uranus", "lune"];
+
+// Enoncé : console.log each item in the array.
+// Traitement
+for (let index = 0; index < planets.length; index++) {
+    // Résultat
+    console.log(planets[index]); 
+    /* 
+        pluton
+        mercure
+        jupiter
+        uranus
+        lune
+    */
+        
+}
+
+// Enoncé : Also console.log the index in each iteration.
+// Traitement
+planets.forEach(function(element, index) {
+    // Résultat
+    console.log(element, index); 
+    /*
+        pluton 0
+        mercure 1
+        jupiter 2
+        uranus 3
+        lune 4
+    */ 
+});
+
+
+
 
 
 
