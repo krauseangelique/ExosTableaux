c7a9ae21cd262ce42c0b60b5f1375fabfc1cf333
krauseangelique <krause_angelique@hotmail.com>
2023-08-04 11:06:11 +0200

    Nouveaux exercices JS

--- script.js
+++ script.js
@@ -1,65 +1,57 @@
 console.log("Ca fonctionne !");
 
-// VARIABLES
-const angi = "Angi"; // Non réassignable & Portée limitée
-let ser;// réassignable & Portée limitée
+// Exercices : Les Tableaux
 
-// Portée des variables
-const firstname = "Lucas"; // script.js:8 Uncaught ReferenceError: Cannot access 'firstname' before initialization
-console.log(firstname);
-let catName = "Lucky";
-console.log(catName);
+// Exo1 : 
 
-function hello() {
-    const lastname = "Ielli";
-    catName = "Luna";
-    console.log(firstname);
-    console.log(lastname);
-    let catAge = 5; // Variable réassignable mais à portée limitée
-    
+
+// Données
+let myAlphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "$",
+    "*",
+    "/",
+    "-",
+    "+",
+  ];
+// Traitement : What is the length of the array?
+console.log(myAlphabet.length); // Résultat 31
+
+// Traitement : Write a function called myAlphabetLength() which console.log the length of the array.
+function myAlphabet() {
+    console.log(myAlphabet.length);     
 }
-hello();
-// console.log(lastname); //N'est pas dispo
 
-// Types de variables
-const stringA = "Salut"; // String = Chaine de caractère = "", '', ``
-const numberA = 0; // Nombre = Number = 0
-let booleanA = true; // Boolean = booléen = true OU false
-const arrayA = []; // Array = tableau = []
-const objectA = {}; // Object = Objet = {}
-
-let conditionConnection = true;
-
-// Les tableaux plus précisément
-const fruits = ['Pomme', 2, {}, undefined, null, [], true];
-console.log(fruits); // Ma variable de type TABLEAUX
-console.log(fruits[0]); // "Pomme" = Type STRING
-console.log(fruits[1]); // 2000 = Type NUMBER
-console.log(fruits[2]); //  {} = Type OBJECT
-console.log(fruits[3]); // undefined = type non défini
-console.log(fruits[4]); // null = nulle = Type null
-console.log(fruits[5]); // [] = type TABLEAU
-console.log(fruits[6]); // true = Type BOOLEEN
+// Appel de fonction
+myAlphabet(); // Résultat 31
 
 
-// Les objets plus précisement
-const chats = {
-    nom: "Lucky",
-    age: 5,
-    couleur: ["Gris", "Blanc", "Tigré"],
-    sexe: "Mâle",
-    sterile: true,
-    puce: undefined
-};
-
-console.log(chats); // OBJETS = OBJECTS
-console.log(chats.nom); // CHAINE DE CARACTERE = STRING
-console.log(chats.age); // NOMBRE = NUMBER
-console.log(chats.couleur); // TABLEAU = ARRAY
-console.log(chats.sexe); // CHAINE DE CARACTERE = STRING
-console.log(chats.sterilise); // BOOLEEN = BOOLEAN
-console.log(chats.puce); // INDEFINI = UNDEFINED
-// Nous allons basculer le projet vers une NOUVELLE branche : branche de developement
+// Exo2 : 
 
 
 
